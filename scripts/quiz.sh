556d9d7253ecf4fd102c5ec8d7cc5b8d6aba5363
Jacob Bendicksen <jacob@jacobbendicksen.com>
2018-02-03 15:46:07 -0500

    http status code check on quiz script

--- scripts/quiz.sh
+++ scripts/quiz.sh
@@ -4,7 +4,7 @@
 # then run with ./quiz.sh
 
 # gets the full list of classes
-curl 'https://www.cpsc213.io/rest/meetings?order=begins_at&select=id,title,slug,begins_at' | jq
+curl -s 'https://www.cpsc213.io/rest/meetings?order=begins_at&select=id,title,slug,begins_at' | jq
 
 # reading in the specific class' id and the user's JWT, if it's not already stored
 read -p "Class id: "  classid
@@ -14,16 +14,26 @@ if [[ -z $JWT ]]; then
 fi
 
 # gets the quiz information for the class
-curl -H "Authorization: Bearer $JWT" "https://www.cpsc213.io/rest/quizzes?meeting_id=eq.$classid" | jq
+curl -s -H "Authorization: Bearer $JWT" "https://www.cpsc213.io/rest/quizzes?meeting_id=eq.$classid" | jq
 
 # reading in the quiz id (not the same as the class id)
 read -p "Quiz id: " quizid
 
+open=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: Bearer $JWT" -X POST -d "quiz_id=$quizid" 'https://www.cpsc213.io/rest/quiz_submissions')
+
+if [[ $open -ne 200 ]]; then
+    echo
+    echo "Can't create submission, exiting"
+    echo "HTTP status code: $open"
+    echo
+    exit 1
+fi
+
 # starts the quiz submission
-curl -H "Authorization: Bearer $JWT" -X POST -d "quiz_id=$quizid" 'https://www.cpsc213.io/rest/quiz_submissions'
+curl -s -H "Authorization: Bearer $JWT" -X POST -d "quiz_id=$quizid" 'https://www.cpsc213.io/rest/quiz_submissions'
 
 # gets the list of questions
-curl -H "Authorization: Bearer $JWT" "https://www.cpsc213.io/rest/quiz_questions?quiz_id=eq.$quizid&select=id,body,quiz_question_options(id,body)" | jq
+curl -s -H "Authorization: Bearer $JWT" "https://www.cpsc213.io/rest/quiz_questions?quiz_id=eq.$quizid&select=id,body,quiz_question_options(id,body)" | jq
 
 echo "Answers: (type quit to quit)"
 echo
@@ -33,7 +43,7 @@ while true
 do
     read answer
     if [[ $answer =~ ^[0-9]+$ ]]; then
-        curl -H "Authorization: Bearer $JWT" -X POST -d "quiz_question_option_id=$answer" 'https://www.cpsc213.io/rest/quiz_answers'
+        curl -s -H "Authorization: Bearer $JWT" -X POST -d "quiz_question_option_id=$answer" 'https://www.cpsc213.io/rest/quiz_answers'
         echo "POSTed answer $answer"
     elif [[ "$answer" == "quit" ]]; then
         exit
